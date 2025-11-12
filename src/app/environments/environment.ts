4787968667d8ae9c03579b86b02bcbff2b7766c2
eduar2gp <eduar2gp@gmail.com>
2025-11-12 13:34:36 -0600

    -google client id

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: false,  
+  baseApiUrl: 'http://localhost:8080',
+
+  //firebaseConfig : {
+  //  apiKey: "AIzaSyB_KSsrTM5FcHrbW16JQGKaChqwjOWjwBQ",
+  //  authDomain: "ecommerce-390118.firebaseapp.com",
+  //  projectId: "ecommerce-390118",
+  //  storageBucket: "ecommerce-390118.firebasestorage.app",
+  //  messagingSenderId: "82480889381",
+  //  appId: "1:82480889381:web:77d24aa958cd3d82d0c9cc"
+  //}
+
+  googleClientId: '82480889381-qkvarn7rm3hfetal5m579v7s9tohteu7.apps.googleusercontent.com',
+  firebaseConfig : {
+    apiKey: "AIzaSyCuVblwRZdudPGDk1c2VPYg6zMMC3g0o3E",
+    authDomain: "helloworld-a6508.firebaseapp.com",
+    databaseURL: "https://helloworld-a6508.firebaseio.com",
+    projectId: "helloworld-a6508",
+    storageBucket: "helloworld-a6508.firebasestorage.app",
+    messagingSenderId: "866576572790",
+    appId: "1:866576572790:web:f364313c8c1ef88911da57"
+  }
+};
